38eb8f81accf0af94e93505f5154f42c2282d4dd
Rafael Bergo <rafaelbergo503@gmail.com>
2021-03-22 15:03:44 -0300

    test

--- Jarvis.py
+++ Jarvis.py
@@ -4,6 +4,8 @@ import speech_recognition as sr
 import wikipedia
 import smtplib
 import webbrowser as wb
+import psutil
+import os
 
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
@@ -61,7 +63,7 @@ def wishme():
     speak('Bem vindo de volta!')
     speak('Estou a sua disposição, como posso ajudar?')
 
-    def sendEmail(to, content):
+def sendEmail(to, content):
         server=smtplib.SMTP('smtp.gmail.com', 587)
         server.ehlo()
         server.starttls()
@@ -69,6 +71,14 @@ def wishme():
         server.login('username@gmail.com', 'password')
         server.sendmail('username@gmail.com', to, content)
         server.close()
+
+def cpu():
+    usage = str(psutil.cpu_percent())
+    speak('O uso de cpu está em'+ usage + '%')
+
+    battery = str(psutil.sensors_battery())
+    speak('A bateria está em')
+    speak(battery.percent)
 
 def TakeCommand():
     r=sr.Recognizer()
@@ -125,13 +135,56 @@ if __name__ == '__main__':
                 speak('Não foi possivel enviar o email')
 
         elif 'procurar no navegador' in query:
-            speak('O que gostaria de pesquisar?')
+            speak('O que gostaria de procurar?')
             browserPath = 'C:\Program Files\... (browser path) %s'
             search = TakeCommand().lower
             wb.get(browserPath).open_new_tab(search + '.com')
 
         elif 'procurar no youtube' in query:
-            speak('O que deseja pesquisar?')
+            speak('O que deseja procurar?')
             search_term = TakeCommand().lower()
             speak('Procurando...')
             wb.open('https//www.youtube.com/results?search_query='+ search_term)
+
+        elif 'procurar no google' in query:
+            speak('O que gostaria de procurar?')
+            search_term = TakeCommand().lower()
+            speak('Procurando...')
+            wb.open('https://www.google.com/search?q='+ search_term)
+
+        elif 'cpu' in query:
+            cpu()
+
+        elif 'saia' in query:
+            speak('Saindo...')
+            quit()
+        
+        elif 'bloco de notas' in query:
+            speak('Abrindo bloco de notas...')
+            notepad = r'C:\Windows\notepad.exe'
+            os.startfile(notepad)
+
+        elif 'escreva uma nota' in query:
+            speak('O que devo escrever?')  
+            notes= TakeCommand()
+            file = open('notas.txt','w')
+            speak('Devo incluir na nota a data e hora?')
+            ans = TakeCommand()
+            if 'sim' in ans:
+                strTime = datetime.datetime.now().strftime('%H:%M%S')
+                file.write(strTime)
+                file.write(':-')
+                file.write(notes)
+                speak('Pronto anotado')
+                file.close()
+            else:
+                file.write(notes)
+                speak('Pronto anotado')
+                file.close()
+
+        elif 'abra a nota' in query:
+            speak('Abrindo a nota')
+            file = open(notes.txt)
+            print(file.read())
+            speak(file.read())
+            file.close()
